fadc27ebc94c87137d281b0b1e1307740834f57f
Felicity Allen <fa9@sanger.ac.uk>
2018-08-14 11:32:55 +0100

    Adding checks on input sequence and pam idx location.

--- indel_prediction/predictor/predict.py
+++ indel_prediction/predictor/predict.py
@@ -29,15 +29,10 @@ def fetchRepReads(genindels_file):
 
 def predictMutations(theta_file, target_seq, pam_idx):
 
-    if target_seq[pam_idx+1:pam_idx+3] != 'GG':
-        raise Exception('Non NGG PAM')
-
     theta, train_set, theta_feature_columns = readTheta(theta_file)
 
     #generate indels
     tmp_genindels_file = 'tmp_genindels_%s_%d.txt' % (target_seq, random.randint(0,100000))
-    print(os.listdir(os.curdir))
-    print(os.environ)
     cmd = INDELGENTARGET_EXE + ' %s %d %s' % (target_seq, pam_idx, tmp_genindels_file)
     print(cmd); subprocess.check_call(cmd.split())
     rep_reads = fetchRepReads(tmp_genindels_file)
@@ -59,6 +54,19 @@ def predictMutations(theta_file, target_seq, pam_idx):
 
 def plot_predictions(theta_file, target_seq, pam_idx, title='Title'):
 
+    if pam_idx < 0 or pam_idx >= (len(target_seq)-3):
+        raise Exception('PAM idx out of range')
+
+    if sum([x in ['A','T','G','C'] for x in target_seq]) != len(target_seq):
+        raise Exception('Sequence must be composed of A,T,G,or C only')
+
+    if len(target_seq) < 20 or pam_idx < 13 or pam_idx > len(target_seq)-7:
+        raise Exception('Sequence too short or PAM too close to edge of sequence (must have at least 10nt either side of cut site)')
+
+    if target_seq[pam_idx+1:pam_idx+3] != 'GG':
+        raise Exception('Non NGG PAM (check correct index of PAM)')
+
+
     profile, rep_reads = predictMutations(theta_file, target_seq, pam_idx)
     profile['-'] = 1000
     rep_reads['-'] = target_seq
@@ -68,11 +76,8 @@ def plot_predictions(theta_file, target_seq, pam_idx, title='Title'):
 if __name__ == '__main__':
     
     theta_file =  'model_output_2000_0.01000000_1.835_theta.txt_cf0.txt'
-    target_seq = 'CTGAGTAGCTATGCGGCCAGCAGCGAGACGCTCAGCGTGAAGCGGCAGTATCCCTCTTTCCTGCGCACCATCCCCAATC'
-    pam_idx = 42
-    profile, rep_reads = predictMutations(theta_file, target_seq, pam_idx)
-    profile['-'] = 1000; rep_reads['-'] = target_seq
-    plotProfiles([profile],[rep_reads],[pam_idx],[False],['Predicted'])
-
+    target_seq = 'GGCCAGCGGAGCATGCATGCAGGGAAGATGAGAGTGATGTAGCAGCGATCGATGCTAGCACG'
+    pam_idx = 20
+    plot_predictions(theta_file, target_seq, pam_idx)
     import pdb; pdb.set_trace()
     
